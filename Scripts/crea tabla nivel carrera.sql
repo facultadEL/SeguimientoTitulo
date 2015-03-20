600408fc534dd62637288bff41b06d95cbfccc1b
Lucas <lpm19.2009@gmail.com>
2015-03-20 18:06:08 -0300

    scripts para crea base de datos local

---
+++ Scripts/crea tabla nivel carrera.sql
@@ -0,0 +1,13 @@
+﻿-- Table: nivel_carrera
+
+-- DROP TABLE nivel_carrera;
+
+CREATE TABLE nivel_carrera
+(
+  id_nivel_carrera serial NOT NULL primary key,
+  nombre_nivel_carrera character varying
+);
+INSERT INTO nivel_carrera(id_nivel_carrera,nombre_nivel_carrera)VALUES(1,'Grado');
+INSERT INTO nivel_carrera(id_nivel_carrera,nombre_nivel_carrera)VALUES(2,'Posgrado');
+INSERT INTO nivel_carrera(id_nivel_carrera,nombre_nivel_carrera)VALUES(3,'Pregrado');
+
---
+++ Scripts/crea tabla numero_nota_rectorado.sql
@@ -0,0 +1,10 @@
+﻿-- Table: numero_nota_rectorado
+
+-- DROP TABLE numero_nota_rectorado;
+
+CREATE TABLE numero_nota_rectorado
+(
+  id_numero_nota_rectorado serial NOT NULL primary key,
+  numero_nota character varying,
+  direccion_nota character varying
+);
