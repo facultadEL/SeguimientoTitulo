a50925329f38d291ffc923e59b24b02e40073918
Lucas <lpm19.2009@gmail.com>
2015-03-20 22:17:02 -0300

    subiendo scripts correctos. tenia un problema de nombres de archivos y no se borraron los que tenia que borrar sino que se borraron los que no debian borrarse

---
+++ Scripts/08.crea_tabla_numero_acta.sql
@@ -0,0 +1,10 @@
+﻿-- Table: numero_acta
+
+-- DROP TABLE numero_acta;
+
+CREATE TABLE numero_acta
+(
+  id_numero_acta serial NOT NULL primary key,
+  numero_acta character varying,
+  direccion_acta character varying
+);
