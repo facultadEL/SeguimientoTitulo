7b0235f61da6539709bb2b890bfa1e4a8d433a48
Eze Olea Figueroa <eze.olea.f@gmail.com>
2015-03-24 11:20:24 -0300

    Terminado sincronizar

--- guardarFechas.php
+++ guardarFechas.php
@@ -3,31 +3,34 @@ error_reporting(E_ALL);
 include_once 'libreriaPhp.php';
 include_once 'conexion.php';
 
-function getDataAlumno($idAlumno)
+function getDataAlumno($idSeguimiento)
 {
+	//Creo la conexion para usarla aca sola y para poder hacer consultas sobre esa
+	$sqlAlumnoCarrera = traerSqlCondicion('alumno_fk,carrera_fk','seguimiento','id_seguimiento='.$idSeguimiento);
+	$rowAlumnoCarrera = pg_fetch_array($sqlAlumnoCarrera);
+	$idAlumno = $rowAlumnoCarrera['alumno_fk'];
+	$idCarrera = $rowAlumnoCarrera['carrera_fk'];
+
+	$conGraduados = pg_connect("host=localhost port=5432 user=extension password=newgenius dbname=graduados") or die("Error de conexion.".pg_last_error());
+	//Busco el proximo id
+	$sqlId = pg_query($conGraduados,'SELECT max(id_alumno) FROM alumno');
+	$rowId = pg_fetch_array($sqlId);
+	$maxId = $rowId['max'] + 1;
 	//Carrera alumno, ver
 	$sqlMandar = "";
+	$sqlTelefonos = "";
 	$sqlAlumnos = traerSqlCondicion('*','alumno','id_alumno='.$idAlumno);
 	$rowAlumno = pg_fetch_array(traerSqlCondicion('*','alumno','id_alumno='.$idAlumno));
-	//Nombre_alumno, apellido_alumno, mail_alumno, facebook_alumno, numerodni_alumno, tipodni_alumno
-	//calle_alumno, perfilacademico_alumno, foto_alumno, carrera_alumno, ancho_final, alto_final
-	//fechanacimiento_alumno,numerocalle_alumno, mail_alumno2,twitter_alumno,provincia_nac_alumno,localidad_nac_alumno,
-	//provincia_trabajo_alumno,localidad_trabajo_alumno,provincia_viviendo_alumno,localidad_viviendo_alumno, perfil_laboral_alumno
-	$sqlMandar "INSERT INTO alumno(nombre_alumno,apellido_alumno,mail_alumno,facebook_alumno,numerodni_alumno,
-		tipodni_alumno,calle_alumno,foto_alumno,aca carrera,ancho_final,alto_final,numerocalle_alumno, mail_alumno2,
-		twitter_alumno, provincia_trabajo_alumno, localidad_trabajo_alumno, provincia_viviendo_alumno, localidad_viviendo_alumno,
-		perfil_laboral_alumno, 
-		) VALUES('$rowAlumno['nombre_alumno']','$rowAlumno['apellido_alumno']','$rowAlumno['mail_alumno']','$rowAlumno['facebook_alumno']',
-		'$rowAlumno['numerodni_alumno']','$rowAlumno['tipodni_alumno']','$rowAlumno['calle_alumno']',null,
-		'$rowAlumno['foto_alumno']',carrera,'$rowAlumno['ancho_final']','$rowAlumno['alto_final']','$rowAlumno['fechanacimiento_alumno']',
-		'$rowAlumno['numerocalle_alumno']','$rowAlumno['mail_alumno2']','$rowAlumno['twitter_alumno']','$rowAlumno['provincia_trabajo_alumno']',
-		'$rowAlumno['localidad_trabajo_alumno']','$rowAlumno['provincia_viviendo_alumno']','$rowAlumno['localidad_viviendo_alumno']',
-		'$rowAlumno['perfil_laboral_alumno']','$rowAlumno['nro_legajo']','$rowAlumno['password_alumno']',
-		'$rowAlumno['numerodni_alumno']','$rowAlumno['fechanacimiento_alumno']','$rowAlumno['cp_alumno']',
-		'$rowAlumno['dpto_alumno']','$rowAlumno['caracteristicaf_alumno']','$rowAlumno['telefono_alumno']',
-		'$rowAlumno['caracteristicac_alumno']','$rowAlumno['celular_alumno']','$rowAlumno['cp_alumno2']',
-		'$rowAlumno['empresa_trabaja_alumno']','$rowAlumno['piso_alumno']','$rowAlumno['localidad_nacimiento_alumno']',
-		'$rowAlumno['ultima_materia_alumno']','$rowAlumno['fecha_ultima_mat_alumno']);";
+	$sqlMandar = "INSERT INTO alumno(id_alumno,nombre_alumno,apellido_alumno,mail_alumno,facebook_alumno,numerodni_alumno,tipodni_alumno,calle_alumno,perfilacademico_alumno,foto_alumno,carrera_alumno,ancho_final,alto_final,fechanacimiento_alumno,numerocalle_alumno, mail_alumno2,twitter_alumno, provincia_nac_alumno, localidad_nac_alumno,provincia_trabajo_alumno, localidad_trabajo_alumno,provincia_viviendo_alumno, localidad_viviendo_alumno,perfil_laboral_alumno) VALUES($maxId,'$rowAlumno['nombre_alumno']','$rowAlumno['apellido_alumno']','$rowAlumno['mail_alumno']','$rowAlumno['facebook_alumno']','$rowAlumno['numerodni_alumno']','$rowAlumno['tipodni_alumno']','$rowAlumno['calle_alumno']',null,'$rowAlumno['foto_alumno']',$idCarrera,'$rowAlumno['ancho_final']','$rowAlumno['alto_final']','$rowAlumno['fechanacimiento_alumno']','$rowAlumno['numerocalle_alumno']','$rowAlumno['mail_alumno2']','$rowAlumno['twitter_alumno']','$rowAlumno['provincia_nacimiento_alumno']','$rowAlumno['localidad_nacimiento_alumno']','$rowAlumno['provincia_trabajo_alumno']','$rowAlumno['localidad_trabajo_alumno']','$rowAlumno['provincia_viviendo_alumno']','$rowAlumno['localidad_viviendo_alumno']','$rowAlumno['perfil_laboral_alumno']');";
+	
+	while($rowTelefonos = pg_fetch_array(traerSqlCondicion('*','telefono_del_alumno','alumno_fk='.$idAlumno)))
+	{
+		$sqlTelefonos .= "INSERT INTO telefono_del_alumno(caracteristica_alumno, telefono_alumno, duenio_del_telefono, alumno_fk) VALUES('$rowTelefonos['caracteristica_alumno']','$rowTelefonos['telefono_alumno']','$rowTelefonos['duenio_del_telefono']',$maxId);";
+	}
+
+	$sqlMandar .= $sqlTelefonos;
+	return $sqlMandar;
+
 }
 
 //El separador para el explode siempre es /--/
@@ -41,6 +44,8 @@ $fecha = $vFechaNoFormat[2].'-'.$vFechaNoFormat[1].'-'.$vFechaNoFormat[0];
 $vAlumnosPasar = explode('/--/', $alumnosPasar);
 
 $sqlGuardar = "";
+$controlSincronizar = 0;
+$sqlSincronizar = "";
 $errorPdf = 0;
 
 switch ($etapa){
@@ -138,6 +143,8 @@ switch ($etapa){
 			
 		}
 		$sqlGuardar = $sqlNuevoNroResolucion.$sqlGuardar;
+		$controlSincronizar = 1;
+		$sqlSincronizar = getDataAlumno($idAlumno);
 		$redireccion = 'resolucionCs.php?';
 		break;
 	case 5:
@@ -199,6 +206,19 @@ switch ($etapa){
 
 if($errorPdf == 0){
 	$e = guardarSql($sqlGuardar);
+	if($controlSincronizar == 1)
+	{
+		$conGraduados = pg_connect("host=localhost port=5432 user=extension password=newgenius dbname=graduados") or die("Error de conexion.".pg_last_error());
+		if(!pg_query($conGraduados,$sqlSincronizar)){
+        $termino = "ROLLBACK";
+	        $error = 1;
+	    }else{
+	        $termino = "COMMIT";
+	    }
+    
+    	pg_query($termino);
+	}
+	
 	if($e==1){
 		mostrarMensaje('Los datos no se pudieron guardar. Contactese con su administrador',$redireccion);
 	}else{
