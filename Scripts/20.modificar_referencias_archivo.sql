069f5c1b1c7f58bfa3bb20bd9f88b2f2d9084ee8
Eze Olea Figueroa <eze.olea.f@gmail.com>
2015-12-29 18:45:22 -0300

    Arreglado procesos

--- Scripts/20.modificar_referencias_archivo.sql
+++ Scripts/20.modificar_referencias_archivo.sql
@@ -1,8 +1,8 @@
-ALTER TABLE seguimiento DROP CONSTRAINT num_res_cd_fk_fkey
-ALTER TABLE seguimiento ADD FOREIGN KEY(num_res_cd_fk) REFERENCES archivo(id)
-ALTER TABLE seguimiento DROP CONSTRAINT num_nota_fk_fkey
-ALTER TABLE seguimiento ADD FOREIGN KEY(num_nota_fk) REFERENCES archivo(id)
-ALTER TABLE seguimiento DROP CONSTRAINT num_res_cs_fk_fkey
-ALTER TABLE seguimiento ADD FOREIGN KEY(num_res_cs_fk) REFERENCES archivo(id)
-ALTER TABLE seguimiento DROP CONSTRAINT num_acta_fk_fkey
-ALTER TABLE seguimiento ADD FOREIGN KEY(num_acta_fk) REFERENCES archivo(id)
+ALTER TABLE seguimiento DROP CONSTRAINT num_res_fk_fkey;
+ALTER TABLE seguimiento ADD FOREIGN KEY(num_res_cd_fk) REFERENCES archivo(id);
+ALTER TABLE seguimiento DROP CONSTRAINT num_nota_fk_fkey;
+ALTER TABLE seguimiento ADD FOREIGN KEY(num_nota_fk) REFERENCES archivo(id);
+ALTER TABLE seguimiento DROP CONSTRAINT num_res_cs_fk_fkey;
+ALTER TABLE seguimiento ADD FOREIGN KEY(num_res_cs_fk) REFERENCES archivo(id);
+ALTER TABLE seguimiento DROP CONSTRAINT num_acta_fk_fkey;
+ALTER TABLE seguimiento ADD FOREIGN KEY(num_acta_fk) REFERENCES archivo(id);
--- util/procesoActualizarFechaRespAlumnos.php
+++ util/procesoActualizarFechaRespAlumnos.php
@@ -4,7 +4,7 @@ include_once '../conexion.php';
 
 $sqlGuardar = '';
 
-$sqlSeguimiento = pg_query('SELECT id_seguimiento,fecha_solicitud FROM seguimiento');
+$sqlSeguimiento = pg_query('SELECT id_seguimiento,fecha_solicitud FROM seguimiento WHERE fecha_solicitud IS NOT NULL');
 while($row = pg_fetch_array($sqlSeguimiento))
 {
 	$idSeguimiento = $row['id_seguimiento'];
